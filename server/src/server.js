0cd8320ab2e5cf0ab672acebee21a616443ddb9b
Moustafa Mansour <mous.ahmed@outlook.com>
2022-09-17 10:52:08 +0200

    1- Added react code for the client 2- Created the first endpoint to return all habitabal planets

---
+++ server/src/server.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const app = require("./app");
+const {loadPlanetData} = require("./models/planets.model");
+const PORT = process.env.PORT || 8000;
+const server = http.createServer(app);
+
+async function startServer() {
+	await loadPlanetData();
+	server.listen(PORT, () => {
+		console.log(`Server listening on port ${PORT}`);
+	});
+}
+
+startServer();
