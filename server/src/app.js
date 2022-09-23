82170d4eade733c35686aa6a913b9aeac0f76bb2
Moustafa Mansour <mous.ahmed@outlook.com>
2022-09-23 15:14:45 +0200

    1- Added github actions 2- added the build job to the cicd pipeline

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
+const helmet = require("helmet");
 const api = require("./routes/api");
 const app = express();
 
+app.use(helmet());
 app.use(
 	cors({
 		origin: "http://localhost:3000",
